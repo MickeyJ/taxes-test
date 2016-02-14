45bd759dbad4a7b5dcc43397619c9fb1fb9ad2c1
Mickey <develop.agility@gmail.com>
2016-02-13 20:01:01 -0700

    passing all test - I think its right

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 module.exports = {
 
-  testTax: function(){
-    return true;
+
+  calcTax: function(amount){
+
+
+    if(amount <= 10) {
+
+      return amount / 10;
+
+    }else if (numCheck(10,20)) {
+
+      return calcRate(10,7) +1;
+
+    }else if (numCheck(20,30)) {
+
+      return calcRate(20,5) +2;
+
+    }else {
+
+      return calcRate(30,3) +3;
+    }
+
+
+
+    function numCheck(min, max){
+      return amount > min && amount <= max;
+    }
+
+    function calcRate(limit, percent){
+      return ((amount - (limit % amount)) * percent / 100);
+    }
+
   }
+
 };
+
--- test/test.js
+++ test/test.js
@@ -3,7 +3,21 @@ var expect = require('chai').expect;
 
 describe('test', function(){
 
-  it('it against true', function(){
-    expect(code.testTax()).to.equal(true);
-  })
+  it('should tax 10% on the first $10', function() {
+    expect(code.calcTax(1)).to.equal(0.1);
+    expect(code.calcTax(10)).to.equal(1);
+  });
+
+  it('should tax 7% on the second $10', function(){
+    expect(code.calcTax(15)).to.equal(1.35);
+  });
+
+  it('should tax 5% on the third $10', function(){
+    expect(code.calcTax(25)).to.equal(2.25);
+  });
+
+  it('should tax 3% thereafter', function(){
+    expect(code.calcTax(35)).to.equal(3.15);
+  });
+
 });
